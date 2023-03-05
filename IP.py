e7fb7de85d762942c8fd20d77e94d007f1084722
shriyadh <shriyadh0306@gmail.com>
2023-03-05 18:17:08 -0500

    ip packet

---
+++ IP.py
@@ -0,0 +1,22 @@
+
+import socket
+class IP:
+
+
+
+
+    def __init__(self):
+        pass
+
+
+class IP_Packet:
+
+    def __init__(self):
+        self.version = 4
+        self.ihl = 5
+        self.type_service = 0
+        self.length = 0
+        self.id = 0
+        self.offset - 0
+        self.time_to_live = 0
+        self.protocol = socket.IPPROTO_TCP
