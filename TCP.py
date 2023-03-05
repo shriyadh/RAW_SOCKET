f80fcba559d32cd10a371cc4445e8faf3458279e
shriyadh <shriyadh0306@gmail.com>
2023-03-05 17:56:16 -0500

    tcp packet

--- TCP.py
+++ TCP.py
@@ -11,12 +11,11 @@ class TCP:
         self.server_ip = ''
         self.server_port = 0
         self.sq_num = 0
-        self.ack_num =  0
+        self.ack_num = 0
         # ip socket
-        #cwnd
+        # cwnd
 
     def connect(self, server_ip, server_port):
-
         # destination IP address
         self.server_ip = socket.gethostbyname(server_ip)
 
@@ -24,19 +23,19 @@ class TCP:
         self.server_port = server_port
 
         # get local ip address
-        #self.client_ip =
+        # self.client_ip =
 
         # pick up any random port number which is not reserved
-        self.client_port = randint( 1024, 65353)
-
+        self.client_port = randint(1024, 65353)
 
 
 class TCPPacket:
 
-    def __init__(self, src_port, dest_port):
-
-        self.src_port = src_port
-        self.dest_port = dest_port
+    def __init__(self, data, src_port=0, dest_port=80, src_ip='', dest_ip=''):
+        self.client_ip = src_ip
+        self.client_port = src_port
+        self.server_ip = dest_ip
+        self.server_port = dest_port
         self.seq_num = randint(0, 100000)
         self.ack_num = 0
         self.offset = 5
@@ -51,14 +50,30 @@ class TCPPacket:
         self.ack = 0
         self.urg = 0
 
-
+        self.data = data
 
     def create_TCP_packet(self):
         tcp_offset_res = (self.offset << 4) + 0
         tcp_flags = self.fin + (self.syn << 1) + (self.rst << 2) + (self.psh << 3) + (self.ack << 4) + (self.urg << 5)
 
-        tcp_header = struct.pack('!HHLLBBHHH', self.src_port, self.dest_port, self.seq_num, self.ack_num, tcp_offset_res, tcp_flags,
-                          self.wnd_size,
-                          self.checksum, self.urg_ptr)
+        tcp_header_without_checksum = struct.pack('!HHLLBBHHH', self.client_port, self.server_port, self.seq_num,
+                                                  self.ack_num, tcp_offset_res, tcp_flags,
+                                                  self.wnd_size,
+                                                  self.checksum, self.urg_ptr)
 
-        # pseudo header fields
+        # pseudo header fields from IP header -- should have source IP, Destination IP, Protocol field
+        #  TCP length, TCP header ===== needed for calculating checksum accurately
+        tcp_len = len(self.data) + len(tcp_header_without_checksum)
+        pseudo_header = struct.pack('!4s4sBBH', socket.inet_aton(self.client_ip), socket.inet_aton(self.server_ip),
+                                    0, socket.IPPROTO_TCP, tcp_len)
+
+        final_header = pseudo_header + tcp_header_without_checksum + self.data
+        self.checksum = calculate_checksum(final_header)
+
+        # tcp header with checksum
+        tcp_with_checksum = struct.pack('!HHLLBBHHH', self.client_port, self.server_port, self.seq_num, self.ack_num,
+                                        tcp_offset_res, tcp_flags,
+                                        self.wnd_size, self.checksum, self.urg_ptr)
+
+        # final tcp packet --- header with checksum + data [[[[[[ TCP HEADER + DATA}
+        tcp_segment = tcp_with_checksum + self.data
